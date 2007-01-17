60d94f00c58a4dece5c22d607fcbbd44b72103c7
Max Wiehle <mwiehle@src.gnome.org>
2007-01-17 17:10:17 +0000

    Outch. copied the wrong directory to create the branch. I don't wan't all that attic stuff.
    svn path=/branches/beagle-metadata-branch; revision=3270

--- attic/WebServices/search.asmx
+++
@@ -1,1 +0,0 @@
-<%@ WebService Language="c#" Class="WebService_CodeBehind.BeagleWebService" %>
--- beagle/wrappers/python/fix.pl
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/perl
-
-while (<>) {
-	$file .= $_;
-}
-
-$file =~ s/define-enum (.*?)(\s+)\(gtype-id \"(.*?)\"\)/define-enum $1/gs;
-
-print "$file\n";
