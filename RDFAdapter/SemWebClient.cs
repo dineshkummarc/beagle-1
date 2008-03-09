5fe966f7d5e71583493b16185c059bbb5ce6eed6
Debajyoti Bera <dbera@src.gnome.org>
2008-03-09 00:10:55 +0000

    Add another Euler reasoning and move the common operations to its own functions.
    svn path=/branches/beagle-rdf/; revision=4595

--- RDFAdapter/SemWebClient.cs
+++ RDFAdapter/SemWebClient.cs
@@ -26,9 +26,12 @@
 //
 
 using System;
+using System.IO;
 using Beagle;
 using Beagle.Util;
 using SemWeb;
+using SemWeb.Query;
+using SemWeb.Inference;
 
 public class SemWebClient {
 	public static void Main (string[] args)
@@ -36,10 +39,11 @@ public class SemWebClient {
 		BeagleSource source = new BeagleSource ();
 
 		TestSource (source);
-		TestEmailThreads (source);
+		//TestEmailThreads (source);
+		//TestReceipients (source);
 	}
 
-	private static void TestSource (SelectableSource source)
+	private static void TestSource (BeagleSource source)
 	{
 		source.RDFToBeagle = new BeagleSource.RDFToBeagleHook (RDFToBeagle);
 		source.BeagleToRDF = new BeagleSource.BeagleToRDFHook (EmailToEntity);
@@ -63,15 +67,15 @@ public class SemWebClient {
 	}
 
 	// WARNING! This will generate all threads, pretty expensive
-	private static void TestEmailThreads (SelectableSource source)
+	private static void TestEmailThreads (BeagleSource source)
 	{
 		source.BeagleToRDF = new BeagleSource.BeagleToRDFHook (MsgIdToEntity);
-		Store store = new Store (source);
-
+		source.RDFToBeagle = new BeagleSource.RDFToBeagleHook (RDFToBeagle);
 		Entity inthread = BeagleSource.BeaglePropertyToEntity ("inthread");
 
 		/* NOTE: change the subject to whatever email subject you wish */
 		const string Subject = "beagle";
+
 		string rules = "@prefix : <http://beagle-project.org/property#>.\n" +
 			"\n" +
 			/* every message is in its own thread */
@@ -79,12 +83,43 @@ public class SemWebClient {
 			/* if any email refers to some email in thread, then this email is also in thread */
 			"{ ?ref :inthread ?parent . ?email1 :prop:k:fixme:reference ?ref . ?email1 :prop:k:fixme:msgid ?msg .} => {?msg :inthread ?parent} .\n";
 
+		Statement msg_in_thread = new Statement (new Variable ("msg"), inthread, new Variable ("parent"));
+		Console.WriteLine ("Proof of 'threads in email with subject :{0}'", Subject);
+		EulerQuery (source, rules, new Statement[] {msg_in_thread});
+	}
+
+	private static void TestReceipients (BeagleSource source)
+	{
+		source.BeagleToRDF = new BeagleSource.BeagleToRDFHook (EmailToEntity);
+		source.RDFToBeagle = new BeagleSource.RDFToBeagleHook (RDFToBeagle);
+
+		Entity recvd = BeagleSource.BeaglePropertyToEntity ("recvd");
+
+		string rules = "@prefix : <http://beagle-project.org/property#>.\n" +
+			"\n" +
+			"{ ?email :prop:t:fixme:from_address ?from . ?email :prop:t:fixme:to_address ?to . } => {?from :recvd ?to .}.\n" +
+			"{ ?email :prop:t:fixme:from_address ?from . ?email :prop:t:fixme:cc_address ?to . } => {?from :recvd ?to .}.\n";
+
+		/* NOTE: change the sender to whatever email address you wish */
+		const string Sender = "dbera.web@gmail.com";
+
+		Entity from = new Entity ("mailto://" + Sender);
+		Variable to = new Variable ("recpt");
+		Statement q = new Statement(from, recvd, to);
+
+		Console.WriteLine ("Proof of 'receipients of emails from :{0}'", Sender);
+		EulerQuery (source, rules, new Statement[] {q});
+	}
+
+	private static void EulerQuery (SelectableSource source, string rules, Statement[] queries)
+	{
+		Store store = new Store (source);
+
 		Euler engine = new Euler(new N3Reader(new StringReader(rules)));
 
-		Statement msg_in_thread = new Statement (new Variable ("msg"), inthread, new Variable ("parent"));
-
-		foreach (Proof p in engine.Prove (store, new Statement[] { msg_in_thread })) {
-			Console.WriteLine(p);
+		foreach (Proof p in engine.Prove (store, queries)) {
+			foreach (Statement s in p.Proved)
+				Console.WriteLine(s);
 		}
 	}
 
